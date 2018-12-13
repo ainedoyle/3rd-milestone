57f4927aa682b708667ca0c6fe3eaef03a7553ba
Aine Doyle <doy89@outlook.ie>
2018-12-13 12:55:27 +0000

    New code

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
 import os
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, json
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["GET", "POST"])
 def index():
+    bmi = ''
+    if request.method == 'POST' and 'weight' in request.form:
+        weight = float(request.form.get('weight'))
+        height = float(request.form.get('height'))
+        bmi = calc_bmi(weight, height)
+    return render_template("index.html", bmi=bmi)
     
-    return render_template("index.html")
 
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+
+
+def calc_bmi(weight, height):
+    return round((weight / ((height / 100) ** 2)), 2)
+
+
+app.run(host=os.getenv('IP'), 
+                port=int(os.getenv('PORT')), 
+                debug=True)
 
 
