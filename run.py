9387a5e43d97f0c31a1a3394be869b1fdf29179d
ainedoyle <31050488+ainedoyle@users.noreply.github.com>
2018-12-13 18:52:58 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,28 +1,46 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, json
+from flask import Flask, render_template, request, redirect
 
+# Set to current function
 app = Flask(__name__)
 
 
+# To access page and input information
 @app.route('/', methods=["GET", "POST"])
+#Function
 def index():
+    
     bmi = ''
+    
+    # To grab data
     if request.method == 'POST' and 'weight' in request.form:
+        
+        # Float number
         weight = float(request.form.get('weight'))
+        
+        #Float number
         height = float(request.form.get('height'))
+        
+        #Passing variables to function
         bmi = calc_bmi(weight, height)
+        
+    # To run page
     return render_template("index.html", bmi=bmi)
     
 
 
-
 def calc_bmi(weight, height):
+    #Weight is divided by the height in meters squared
+    #The answer needs to be rounded
     return round((weight / ((height / 100) ** 2)), 2)
 
 
-app.run(host=os.getenv('IP'), 
-                port=int(os.getenv('PORT')), 
-                debug=True)
+
+# To execute the code
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
 
 
